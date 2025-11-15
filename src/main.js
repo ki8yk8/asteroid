c8978113d1b39128555408b0e6fe8812d5365101
ki8yk8 <ki***@gmail.com>
2025-11-16 00:01:33 +0545

    adds content to the gameover screen

--- src/main.js
+++ src/main.js
@@ -40,4 +40,4 @@ registerInstructionScene({ name: "instructions", k, C: CONSTANTS });
 registerGameplayScene({ name: "play", k, C: CONSTANTS });
 registerGameoverScene({ name: "over", k, C: CONSTANTS });
 
-k.go("menu");
+k.go("over", 1);
--- src/scenes/gameover.js
+++ src/scenes/gameover.js
@@ -1,5 +1,47 @@
 export function registerGameoverScene({ name, k, C }) {
 	k.scene(name, (score) => {
-		k.add([k.text("Game over"), k.color(0, 0, 0)]);
+		const title = k.add([
+			k.text("Game over", {
+				size: 48,
+			}),
+			k.anchor("center"),
+			k.pos(k.width() / 2, k.height() / 4),
+		]);
+
+		const score_title = k.add([
+			k.text("Your Scored", {
+				size: 22,
+			}),
+			k.anchor("center"),
+			k.pos(k.width() / 2, title.pos.y + title.height + 28),
+		]);
+
+		const score_number = k.add([
+			k.text(score, {
+				size: 72,
+			}),
+			k.anchor("center"),
+			k.pos(k.width() / 2, score_title.pos.y + score_title.height + 30),
+		]);
+
+		const hint = k.add([
+			k.text("Press space to continue", { size: 26 }),
+			k.anchor("bot"),
+			k.pos(k.width() / 2, k.height() - 100),
+			k.rotate(0),
+			k.scale(1),
+			k.animate({ relative: true }),
+		]);
+
+		hint.animate("angle", [0, -1, 0, 1, 0], {
+			duration: 3,
+		});
+		hint.animate("scale", [1, 1.05, 0.95, 1], {
+			duration: 2,
+		});
+
+		k.onKeyPress("space", () => {
+			k.go("menu");
+		});
 	});
 }
