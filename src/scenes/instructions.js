86023693a3651318bd86baaed7952f93999a01cd
ki8yk8 <ki***@gmail.com>
2025-11-15 23:39:37 +0545

    adds content into insructions screen

--- src/scenes/instructions.js
+++ src/scenes/instructions.js
@@ -1,6 +1,48 @@
+import { createStarBG } from "../objects/stars";
+
 export function registerInstructionScene({ k, name, C }) {
 	k.scene(name, () => {
-		k.add([k.text("Instructions")]);
+		createStarBG({ k, C, num: 100 });
+
+		const title = k.add([
+			k.text("Instructions", {
+				size: 48,
+			}),
+			k.anchor("center"),
+			k.pos(k.width() / 2, 100),
+		]);
+
+		const paragraph = k.add([
+			k.text(
+				"You are green rocket. And your goal is to protect against asteroids. You have your boundary and if you leave that your health decreases. \n\nYou can increase your speed by pressing 's' and you can shoot to asteroids using space key.",
+				{
+					size: 22,
+					width: 400,
+					lineSpacing: 10,
+					align: "center",
+				}
+			),
+			k.anchor("top"),
+			k.pos(k.width() / 2, title.pos.y + title.height / 2 + 32),
+		]);
+
+		const hint = k.add([
+			k.text("Press space to continue", {
+				size: 26,
+			}),
+			k.anchor("top"),
+			k.pos(k.width() / 2, paragraph.pos.y + paragraph.height + 100),
+			k.scale(1),
+			k.rotate(0),
+			k.animate({ relative: true }),
+		]);
+
+		hint.animate("angle", [0, -2, 0, 2, 0], {
+			duration: 2,
+		});
+		hint.animate("scale", [1, 1.1, 0.9, 1], {
+			duration: 1,
+		});
 	});
 
 	k.onKeyPress("space", () => {
