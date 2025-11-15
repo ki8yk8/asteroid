a6fc67ff7d9639a6f7397a6efe7160e8b8c8dcf5
ki8yk8 <ki***@gmail.com>
2025-11-15 17:05:11 +0545

    implementes getting random position around the screen boundary for asteroid

--- src/objects/asteroid.js
+++ src/objects/asteroid.js
@@ -4,10 +4,31 @@ const ASTEROID_SIZE = {
 	large: 0.3,
 };
 
-export default function Asetroid({ k, C, size = "medium" }) {
-	const w = ASTEROID_SIZE[size];
+function get_random_screen_pos(k) {
+	// valid positions are around the screen
+	// [x, 0], [0, y], [width, y], [0, height]
+	const positions = [
+		[k.rand(0, k.width()), 0],
+		[0, k.rand(0, k.height())],
+		[k.width(), k.rand(0, k.height())],
+		[k.rand(0, k.width()), k.height()],
+	];
 
-	const asteroid = k.add([k.sprite("asteroid"), k.scale(0.3)]);
+	return k.choose(positions);
+}
+
+export default function Asetroid({ k, C, size = "medium" }) {
+	const s = ASTEROID_SIZE[size];
+
+	const rnd_pos = get_random_screen_pos(k);
+
+	const asteroid = k.add([
+		k.sprite("asteroid"),
+		k.pos(rnd_pos[0], rnd_pos[1]),
+		k.scale(s),
+		k.anchor("center"),
+		"asteroid",
+	]);
 
 	return asteroid;
 }
