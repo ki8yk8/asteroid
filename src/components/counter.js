53a174b2db9e65e507a3e2cffe94e4f5955fba34
ki8yk8 <ki***@gmail.com>
2025-11-15 21:32:45 +0545

    makes the counter value dynamic

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,15 +1,10 @@
-export function Counter({ k, C, props }) {
-	const counter = k.add([
-		k.rect(200, 50),
-		k.pos(props.pos[0], props.pos[1]),
-		k.anchor(props.anchor ?? "center"),
-		k.color(255, 255, 255),
-		k.layer("ui"),
-	]);
-
-	const [total_bars, gap] = [20, 1];
-	const bar_width =
-		(counter.width - 2 * 5 - (total_bars - 1) * gap) / total_bars;
+function createBar({ k, counter }) {
+	const MAX_BARS = 20;
+	const [total_bars, gap] = [
+		Math.min(Math.ceil((counter.data / 100) * MAX_BARS), MAX_BARS),
+		1,
+	];
+	const bar_width = (counter.width - 2 * 5 - (MAX_BARS - 1) * gap) / MAX_BARS;
 
 	for (let i = 0; i < total_bars; i++) {
 		counter.add([
@@ -17,8 +12,45 @@ export function Counter({ k, C, props }) {
 			k.color(0, 0, 0),
 			k.pos(-counter.width + bar_width + 5 + i * (gap + bar_width), 5),
 			k.anchor("topright"),
+			"bars",
 		]);
 	}
+}
+
+export function Counter({ k, C, props }) {
+	let previous = props.initial ?? 100;
+	const top = k.add([
+		k.text(props.title, {
+			size: 16,
+		}),
+		k.pos(props.pos[0] - 5, props.pos[1]),
+		k.anchor("topright"),
+	]);
+
+	const counter = k.add([
+		k.rect(200, 50),
+		k.pos(props.pos[0], props.pos[1] + top.height + 5),
+		k.anchor(props.anchor ?? "center"),
+		k.color(255, 255, 255),
+		k.layer("ui"),
+	]);
+	counter.data = previous;
+
+	createBar({ k, counter });
+
+	k.onUpdate(() => {
+		if (counter.data !== previous) {
+			previous = counter.data;
+
+			// delete all the bars
+			counter.get("bars").forEach((bar) => {
+				k.destroy(bar);
+			});
+
+			// create the new bars
+			createBar({ k, counter });
+		}
+	});
 
 	return counter;
 }
--- src/objects/ui.js
+++ src/objects/ui.js
@@ -19,16 +19,6 @@ export default function UI({ k, C }) {
 		k.color(255, 255, 255),
 	]);
 
-	const health_score = UI.add([
-		k.text("health", {
-			size: 24,
-		}),
-		k.color(0, 0, 0),
-		k.pos(k.width() - C.padding[1] / 2, C.padding[0] / 2),
-		k.anchor("topright"),
-		k.color(255, 255, 255),
-	]);
-
 	const counter_pos = [k.width() - C.padding[1] / 2, C.padding[0] / 2];
 	const anchor = "topright";
 	const counter = Counter({
@@ -37,6 +27,8 @@ export default function UI({ k, C }) {
 		props: {
 			anchor,
 			pos: counter_pos,
+			title: "Health",
+			initial: 100,
 		},
 	});
 
