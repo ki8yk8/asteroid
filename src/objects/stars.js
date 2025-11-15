7f3881de5850ed3a8f9e2252e4b0dbbdc5faa968
ki8yk8 <ki***@gmail.com>
2025-11-15 17:32:14 +0545

    creates random star background

---
+++ src/objects/stars.js
@@ -0,0 +1,29 @@
+const COLOR = {
+	light: [139, 133, 169],
+	dark: [54, 29, 183],
+};
+export default function Star({ k, C, type, pos }) {
+	const star = k.add([
+		k.rect(6, 6, {
+			radius: 3,
+		}),
+		k.color(...COLOR[type]),
+		k.pos(...pos),
+		k.anchor("center"),
+	]);
+
+	return star;
+}
+
+export function createStarBG({ k, C }) {
+	const N = 100;
+
+	for (let i = 0; i < N; i++) {
+		Star({
+			k,
+			C,
+			type: k.choose(["light", "dark"]),
+			pos: [k.rand(0, k.width()), k.rand(0, k.height())],
+		});
+	}
+}
--- src/scenes/gameplay.js
+++ src/scenes/gameplay.js
@@ -1,5 +1,6 @@
 import Asetroid from "../objects/asteroid";
 import Rocket from "../objects/rocket";
+import { createStarBG } from "../objects/stars";
 
 export function registerGameplayScene({ name, k, C }) {
 	k.scene(name, () => {
@@ -11,16 +12,14 @@ export function registerGameplayScene({ name, k, C }) {
 			k.anchor("center"),
 			k.pos(k.width() / 2, k.height() / 2),
 			k.color(...Object.values(C.background_color)),
-			k.outline(4, {r: 255, g: 157, b: 24}),
+			k.outline(4, { r: 255, g: 157, b: 24 }),
 			k.area(),
 			"boundary",
 		]);
 
-		console.log(k.RED);
-
 		const rocket = Rocket({ k, C });
-		// Asetroid({ k, C, size: "small" });
-		// Asetroid({ k, C, size: "medium" });
-		Asetroid({ k, C, size: "large" });
+		Asetroid({ k, C, size: "small" });
+
+		createStarBG({ k, C });
 	});
 }
