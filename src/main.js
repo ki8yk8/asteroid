1b1ae83d585c18bfccaba5fe2430e5c6033b0c8d
ki8yk8 <ki***@gmail.com>
2025-11-15 10:08:18 +0545

    adds boundary for player movement

--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@ import { registerMenuScene } from "./scenes/menu";
 import { registerGameoverScene } from "./scenes/gameover";
 import { registerGameplayScene } from "./scenes/gameplay";
 
-const k = kaplay();
+const k = kaplay({ global: false, background: [255, 255, 255] });
 
 k.loadRoot("./");
 
-const CONSTANTS = {};
+console.log(k);
+
+const CONSTANTS = {
+	padding: [50, 100],
+};
 
 registerMenuScene({ name: "menu", k, C: CONSTANTS });
 registerGameplayScene({ name: "play", k, C: CONSTANTS });
 registerGameoverScene({ name: "over", k, C: CONSTANTS });
+
+k.go("play");
--- src/scenes/gameplay.js
+++ src/scenes/gameplay.js
@@ -1,5 +1,13 @@
 export function registerGameplayScene({ name, k, C }) {
 	k.scene(name, () => {
-		k.add([k.text("Game play"), k.color(0, 0, 0)]);
+		const boundary = k.add([
+			k.rect(k.width() - C.padding[1], k.height() - C.padding[0], {
+				radius: Math.min(k.width() - C.padding[1], k.height() - C.padding[0])/2,
+			}),
+			// k.ellipse(k.width() - C.padding[1], k.height() - C.padding[0]), this is not working debug when internet comes,
+			k.anchor("center"),
+			k.pos(k.width()/2, k.height()/2),
+			k.outline(4, k.color(255, 0, 0)),
+		]);
 	});
 }
