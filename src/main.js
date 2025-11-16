a44f3be004e5baf94a143a4e80c761eb4356d866
ki8yk8 <ki***@gmail.com>
2025-11-16 14:37:57 +0545

    loading screen loads the content

--- src/main.js
+++ src/main.js
@@ -9,26 +9,6 @@ const k = kaplay({ global: false, background: [61, 0, 129] });
 
 k.loadRoot("./");
 
-k.loadSprite("asteroid", "/sprites/asteroid.png");
-k.loadSprite("asteroid-1", "/sprites/asteroids/1.png");
-k.loadSprite("asteroid-2", "/sprites/asteroids/2.png");
-k.loadSprite("asteroid-3", "/sprites/asteroids/3.png");
-k.loadSprite("asteroid-4", "/sprites/asteroids/4.png");
-k.loadSprite("asteroid-5", "/sprites/asteroids/5.png");
-k.loadSprite("asteroid-6", "/sprites/asteroids/6.png");
-k.loadSprite("asteroid-7", "/sprites/asteroids/7.png");
-k.loadSprite("asteroid-8", "/sprites/asteroids/8.png");
-k.loadSprite("asteroid-9", "/sprites/asteroids/10.png");
-k.loadSprite("star", "/sprites/star.png");
-k.loadSprite("booster", "/sprites/booster.png");
-k.loadSprite("life", "/sprites/life.png");
-
-k.loadSound("blast", "/sounds/blast.mp3");
-k.loadSound("click", "/sounds/click.mp3");
-k.loadSound("earth", "/sounds/earth.mp3");
-k.loadSound("laser", "/sounds/laser.mp3");
-k.loadSound("space", "/sounds/space.mp3");
-
 const CONSTANTS = {
 	padding: [50, 100],
 	rocket_speed: 100,
--- src/scenes/loader.js
+++ src/scenes/loader.js
@@ -2,8 +2,85 @@ import Asetroid, { createAsteroidBG } from "../objects/asteroid";
 import { createStarBG } from "../objects/stars";
 
 export function registerLoaderScene({ k, name, C }) {
-	k.scene(name, () => {
+	k.scene(name, async () => {
 		createStarBG({ k, C, num: 50 });
-		createAsteroidBG({k, C, num: 10});
+
+		const sprites = [
+			{
+				name: "asteroid",
+				path: "/sprites/asteroid.png",
+			},
+			{
+				name: "asteroid-1",
+				path: "/sprites/asteroids/1.png",
+			},
+			{
+				name: "asteroid-2",
+				path: "/sprites/asteroids/2.png",
+			},
+			{
+				name: "asteroid-3",
+				path: "/sprites/asteroids/3.png",
+			},
+			{
+				name: "asteroid-4",
+				path: "/sprites/asteroids/4.png",
+			},
+			{
+				name: "asteroid-5",
+				path: "/sprites/asteroids/5.png",
+			},
+			{
+				name: "asteroid-6",
+				path: "/sprites/asteroids/6.png",
+			},
+			{
+				name: "asteroid-7",
+				path: "/sprites/asteroids/7.png",
+			},
+			{
+				name: "asteroid-8",
+				path: "/sprites/asteroids/8.png",
+			},
+			{
+				name: "asteroid-9",
+				path: "/sprites/asteroids/10.png",
+			},
+			{
+				name: "star",
+				path: "/sprites/star.png",
+			},
+			{
+				name: "booster",
+				path: "/sprites/booster.png",
+			},
+			{
+				name: "life",
+				path: "sprites/life.png",
+			},
+		];
+
+		const sounds = ["blast", "click", "earth", "laser", "space"];
+
+		let total_loaded = 0;
+
+		sprites.forEach(async (item) => {
+			const {name, path} = item;
+			await k.loadSprite(name, path);
+			handleItemLoaded();
+		});
+
+		sounds.forEach(async (name) => {
+			await k.loadSound(name, `/sounds/${name}.mp3`);
+			handleItemLoaded();
+		});
+
+		function handleItemLoaded() {
+			total_loaded++;
+
+			if (total_loaded === sounds.length + sprites.length) {
+				k.wait(2, () => k.go("menu"));
+			}
+		}
 	});
 }
